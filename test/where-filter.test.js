28979055cdcf8ec16727ba4ae5778d8f8d631737
kolach <chikolad@gmail.com>
2022-11-02 10:29:48 -0500

    feat: test Date and numbers

--- test/where-filter.test.js
+++ test/where-filter.test.js
@@ -426,4 +426,80 @@ context('whereFilter', () => {
 			expect(result).to.have.lengthOf(3);
 		});
 	});
+
+
+	describe('date handling if object fileds are numbers and filter value is Date', () => {
+		const date = DateTime.local();
+		const dateFixtures = [
+			{createdAt: date.minus({days: 1}).toJSDate().getTime()},
+			{createdAt: date.minus({seconds: 1}).toJSDate().getTime()},
+			{createdAt: date.toJSDate().getTime()},
+			{createdAt: date.plus({seconds: 1}).toJSDate().getTime()},
+			{createdAt: date.plus({days: 1}).toJSDate().getTime()},
+		];
+
+		it('should compare equivalent dates correctly', () => {
+			const result = whereFilter({createdAt: date.toJSDate()})({createdAt: date.toJSDate().getTime()});
+
+			expect(result).to.be.true;
+		});
+
+		it('should filter "less than" dates correctly', () => {
+			const result = dateFixtures.filter(whereFilter({createdAt: {lt: date.toJSDate()}}));
+			expect(result).to.have.lengthOf(2);
+		});
+
+		it('should filter "less than or equal to" dates correctly', () => {
+			const result = dateFixtures.filter(whereFilter({createdAt: {lte: date.toJSDate()}}));
+			expect(result).to.have.lengthOf(3);
+		});
+
+		it('should filter "greater than" dates correctly', () => {
+			const result = dateFixtures.filter(whereFilter({createdAt: {gt: date.toJSDate()}}));
+			expect(result).to.have.lengthOf(2);
+		});
+
+		it('should filter "greater than or equal to" dates correctly', () => {
+			const result = dateFixtures.filter(whereFilter({createdAt: {gte: date.toJSDate()}}));
+			expect(result).to.have.lengthOf(3);
+		});
+	});
+
+
+	describe('date handling when object field is Date and filter value is number', () => {
+		const date = DateTime.local();
+		const dateFixtures = [
+			{createdAt: date.minus({days: 1}).toJSDate()},
+			{createdAt: date.minus({seconds: 1}).toJSDate()},
+			{createdAt: date.toJSDate()},
+			{createdAt: date.plus({seconds: 1}).toJSDate()},
+			{createdAt: date.plus({days: 1}).toJSDate()},
+		];
+
+		it('should compare equivalent dates correctly', () => {
+			const result = whereFilter({createdAt: date.toJSDate().getTime()})({createdAt: date.toJSDate()});
+
+			expect(result).to.be.true;
+		});
+
+		it('should filter "less than" dates correctly', () => {
+			const result = dateFixtures.filter(whereFilter({createdAt: {lt: date.toJSDate().getTime()}}));
+			expect(result).to.have.lengthOf(2);
+		});
+
+		it('should filter "less than or equal to" dates correctly', () => {
+			const result = dateFixtures.filter(whereFilter({createdAt: {lte: date.toJSDate().getTime()}}));
+			expect(result).to.have.lengthOf(3);
+		});
+
+		it('should filter "greater than" dates correctly', () => {
+			const result = dateFixtures.filter(whereFilter({createdAt: {gt: date.toJSDate().getTime()}}));
+			expect(result).to.have.lengthOf(2);
+		});
+
+		it('should filter "greater than or equal to" dates correctly', () => {
+			const result = dateFixtures.filter(whereFilter({createdAt: {gte: date.toJSDate().getTime()}}));
+			expect(result).to.have.lengthOf(3);
+		});
+	});
 });
